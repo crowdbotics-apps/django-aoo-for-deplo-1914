3b0d749bad896ca958cec4a607ef2b39c31736f7
Crowdbotics <team@crowdbotics.com>
2020-03-25 15:36:33 +0000

    Update models

--- django_aoo_for_deplo_1914/wsgi.py
+++ django_aoo_for_deplo_1914/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_aoo_for_deplo_1914.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_aoo_for_deplo_1914.settings")
 
 application = get_wsgi_application()
